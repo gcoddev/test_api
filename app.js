554eb68f56ca17a8340a636ac2e4c5d251254376
gcoddev <geco.yak77@gmail.com>
2022-08-23 09:16:01 -0400

    add nodemon - change api_test db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'test_api'
+  database: 'api_test'
 })
 
 app.get('/', (req, res) => {
